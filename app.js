5567f3ed65232f4045993d5a32d4082934a2f7d7
0x3n0 <76135196+0x3n0@users.noreply.github.com>
2022-10-07 21:37:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
     consumer_secret: process.env.TWITTER_API_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    triggerWord: process.env.TRIGGER
   });
 
 
